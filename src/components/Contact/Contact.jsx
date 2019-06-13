efab713083fa72223438bed3e98c6f39227c8f17
Caio Batista <caiobatis7@gmail.com>
2019-06-13 18:54:59 -0300

    add build from gitignore

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -30,7 +30,7 @@ class Contact extends Component {
             <div className="col-md-6">
               <iframe 
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3646.234608717478!2d-46.32996308476481!3d-23.952142682024785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce03745d931789%3A0xe39d01e5e54271ad!2sAv.+Senador+Feij%C3%B3%2C+686+-+Encruzilhada%2C+Santos+-+SP%2C+11050-220!5e0!3m2!1sen!2sbr!4v1559266884654!5m2!1sen!2sbr"
-              width="100%" height="450" frameborder="0" allowfullscreen></iframe>
+              width="100%" height="450" frameBorder="0" allowFullScreen></iframe>
             </div>
             <div className="col-md-1"></div>
             <div className="col-md-5">
