4a5cd8a71ae168883001ebb7b71f3b9894e28641
Caio Batista <caiobatis7@gmail.com>
2019-06-13 18:56:28 -0300

    add build from gitignore

---
+++ build/precache-manifest.24ebc13f3ec7beb565a62da06af24c8a.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "46079578ee457d55ffa5f9b045ab1149",
+    "url": "/static/media/CAR_13.46079578.png"
+  },
+  {
+    "revision": "7a2cf34e8441125ae072",
+    "url": "/static/css/main.867ff2ef.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a36cfdc1fb1b55c971a",
+    "url": "/static/js/2.8b2c6205.chunk.js"
+  },
+  {
+    "revision": "cc29a17426c4def674871f8d8413ee53",
+    "url": "/static/media/sprite.cc29a174.png"
+  },
+  {
+    "revision": "f0d4de0cd08a95e61c1e0e4cc1539a07",
+    "url": "/static/media/are-you-confident-on-the-roads.f0d4de0c.jpg"
+  },
+  {
+    "revision": "66ac9477399635837da45a4d0cd536ae",
+    "url": "/static/media/CAR_1.66ac9477.png"
+  },
+  {
+    "revision": "7a7657dc4841fff5a06e619fdf066a30",
+    "url": "/static/media/CAR_2.7a7657dc.png"
+  },
+  {
+    "revision": "3cdb8028cae7f7f0782bb7b40ed72e5b",
+    "url": "/static/media/CAR_3.3cdb8028.png"
+  },
+  {
+    "revision": "f6df922c40213f7a38ab9972d1da50cc",
+    "url": "/static/media/CAR_4.f6df922c.png"
+  },
+  {
+    "revision": "61ec6792a27b312fd18363657634c7d2",
+    "url": "/static/media/CAR_5.61ec6792.png"
+  },
+  {
+    "revision": "19f6db6936e8cc74f171c0b6305ce545",
+    "url": "/static/media/CAR_6.19f6db69.png"
+  },
+  {
+    "revision": "06f4586e82dcd71aaf93b191d2750707",
+    "url": "/static/media/CAR_7.06f4586e.png"
+  },
+  {
+    "revision": "dfde2989a4200a6da4458caa50a23c3f",
+    "url": "/static/media/CAR_8.dfde2989.png"
+  },
+  {
+    "revision": "7f6ea1a58ce7b177049f8874bee7e891",
+    "url": "/static/media/CAR_9.7f6ea1a5.png"
+  },
+  {
+    "revision": "8ff377b30649b8e198ac5ba780b4889d",
+    "url": "/static/media/CAR_10.8ff377b3.png"
+  },
+  {
+    "revision": "f8570c2e469d8e2b45c76566df44c81a",
+    "url": "/static/media/CAR_11.f8570c2e.png"
+  },
+  {
+    "revision": "a2925b956283e3609a4231b151f5a0c6",
+    "url": "/static/media/CAR_12.a2925b95.png"
+  },
+  {
+    "revision": "7a2cf34e8441125ae072",
+    "url": "/static/js/main.31535d13.chunk.js"
+  },
+  {
+    "revision": "e9d1c704cbb84d2e7dcb8c25ef9dad6a",
+    "url": "/static/media/CAR_14.e9d1c704.png"
+  },
+  {
+    "revision": "40ed7ecd729bac874f81b301fbec5913",
+    "url": "/static/media/CAR_15.40ed7ecd.png"
+  },
+  {
+    "revision": "ae7e5643bb17eecbe2b2a1daac1ec8d8",
+    "url": "/static/media/CAR_16.ae7e5643.png"
+  },
+  {
+    "revision": "f89634c5e2393c4d2933f5d2ac2f6019",
+    "url": "/static/media/CAR_17.f89634c5.png"
+  },
+  {
+    "revision": "42fd6402f83eb120ed3bea754d99e097",
+    "url": "/static/media/CAR_18.42fd6402.png"
+  },
+  {
+    "revision": "7dac0904e45d64472b9cccf7ad341e84",
+    "url": "/static/media/CAR_19.7dac0904.png"
+  },
+  {
+    "revision": "08f9298095a6b1d2f768b352b2a61f36",
+    "url": "/static/media/CAR_20.08f92980.png"
+  },
+  {
+    "revision": "58ee9fa2041d3029cf7d4edd52223587",
+    "url": "/static/media/CAR_21.58ee9fa2.png"
+  },
+  {
+    "revision": "3f895b6135e36b29c5cb166b4ac35bd0",
+    "url": "/static/media/CAR_22.3f895b61.png"
+  },
+  {
+    "revision": "7438dbee900df9f6ac6aece9da589eb7",
+    "url": "/static/media/CAR_23.7438dbee.png"
+  },
+  {
+    "revision": "819a556db8e376b3f0299429f3d2f45f",
+    "url": "/static/media/CAR_24.819a556d.png"
+  },
+  {
+    "revision": "fa9c3915c2fd0ad5b8500cc1bc309866",
+    "url": "/static/media/CAR_25.fa9c3915.png"
+  },
+  {
+    "revision": "ad85caca135af5009f8a8f6ad930eb3f",
+    "url": "/static/media/CAR_26.ad85caca.png"
+  },
+  {
+    "revision": "5e7c4d7e4fc8a90eb6244a9bfe79cc1f",
+    "url": "/static/media/CAR_27.5e7c4d7e.png"
+  },
+  {
+    "revision": "777399cdfe722653d2a6ac31300af868",
+    "url": "/static/media/CAR_28.777399cd.png"
+  },
+  {
+    "revision": "6517ffbec609da7cf56ee95876e2a040",
+    "url": "/static/media/CAR_29.6517ffbe.png"
+  },
+  {
+    "revision": "c8b968d47bb0f99cbebb75f896ab04d0",
+    "url": "/index.html"
+  }
+];
